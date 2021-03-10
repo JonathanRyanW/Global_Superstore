4087a3c7fe84745cf2cf8c17f3928f1de5467b5c
Jonathan Ryan <jonathanryan576@gmail.com>
2021-03-10 16:50:12 +0700

    Initial Commit. Order sheet read.

---
+++ README.R
@@ -0,0 +1,8 @@
+library(xlsx)
+order <- read.xlsx("Global Superstore.xls", sheetIndex = 1, header = TRUE)
+
+"The data reading process takes sooo long i completed 3 course videos while
+waiting. Can my laptop even bear reading another sheet? The RAM is already 88%
+used."
+
+names(order)
